3c5dcfc4aeb871f8f55e8f76e819f370637e3856
Anne Fortuin <phortuin@gmail.com>
2020-03-21 10:29:22 +0100

    refactor: simplify markdown storage process

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const getDropboxFiles = require('./lib/dropbox-get-files')
-const processDropboxFile = require('./lib/dropbox-process-file')
+const contentStore = require('./lib/content-store')
 const markdownToHTML = require('./lib/markdown-to-html')
 const writeToTarget = require('./lib/file-write-to-target')
 
@@ -13,6 +13,18 @@ const INVALID_OR_MISSING_TOKEN = 'Dropbox token seems missing or invalid'
 
 function validToken(token) {
 	return /^[a-zA-Z0-9_-]{64}$/.test(token)
+}
+
+function storeMarkdown(dropboxFile) {
+	if (file.is_text) {
+		try {
+			const markdown = file.fileBinary.toString('utf8')
+			contentStore.setContent(file.path_lower, markdown)
+		} catch(error) {
+			console.error(error)
+		}
+	}
+	return file
 }
 
 const create = () => {
@@ -29,8 +41,8 @@ const create = () => {
 
 	function startTheThing(token, options) {
 		return getDropboxFiles(token)
-			.map(processDropboxFile(options.binariesTarget))
-			.map(markdownToHTML(options.template, options.binariesTarget)) // moet pas als alle dropbox files klaar zijn zodat alle content in contentstore zit
+			.map(storeMarkdown)
+			.map(markdownToHTML(options.template, options.binariesTarget))
 			.map(writeToTarget(options.target))
 	}
 
