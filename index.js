e8f8902a550d44ad0eb07d79ed65071489ac5374
Anne Fortuin <phortuin@gmail.com>
2020-04-02 15:50:31 +0200

    refactor: part 1 of big refactor to dropbox file class

--- index.js
+++ index.js
@@ -1,53 +1,23 @@
 const getDropboxFiles = require('./lib/dropbox-get-files')
-const contentStore = require('./lib/content-store')
-const markdownToHTML = require('./lib/markdown-to-html')
-const writeToTarget = require('./lib/file-write-to-target')
 
 const DEFAULTS = {
 	template: 'index.njk',
-	target: 'build',
-	binariesTarget: 'media',
+	buildFolder: 'build',
+	assetsFolder: 'assets',
 }
-
-const INVALID_OR_MISSING_TOKEN = 'Dropbox token seems missing or invalid'
 
 function validToken(token) {
 	return /^[a-zA-Z0-9_-]{64}$/.test(token)
 }
 
-function storeMarkdown(dropboxFile) {
-	if (file.is_text) {
-		try {
-			const markdown = file.fileBinary.toString('utf8')
-			contentStore.setContent(file.path_lower, markdown)
-		} catch(error) {
-			console.error(error)
-		}
+function dmssg(token, options) {
+	options = Object.assign({}, DEFAULTS, options)
+	token || (token = process.env.DROPBOX_TOKEN)
+	if (!validToken(token)) {
+		throw new Error('Dropbox token seems missing or invalid')
 	}
-	return file
+	return getDropboxFiles(token, options)
+		.map(file => file.writeToTarget())
 }
 
-const create = () => {
-
-	function dmssg(token, options) {
-		options = Object.assign({}, DEFAULTS, options)
-		token || (token = process.env.DROPBOX_TOKEN)
-		if (!validToken(token)) {
-			throw new Error(INVALID_OR_MISSING_TOKEN)
-		}
-		return startTheThing(token, options)
-	}
-
-
-	function startTheThing(token, options) {
-		return getDropboxFiles(token)
-			.map(storeMarkdown)
-			.map(markdownToHTML(options.template, options.binariesTarget))
-			.map(writeToTarget(options.target))
-	}
-
-	return dmssg
-
-}
-
-module.exports = create()
+module.exports = dmssg
